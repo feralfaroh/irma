02a8685e10c813d1840a22575af5bede19d0a666
Fernando Alfaro <122326590+feralfaroh@users.noreply.github.com>
2025-03-05 17:52:42 -0600

    instalar desde scripy

--- function_app.py
+++ function_app.py
@@ -2,6 +2,9 @@ import azure.functions as func
 import logging
 import subprocess
 import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'vendored'))
 
 app = func.FunctionApp()
 
