a9f18f21699e0b4d245259056094b2d42a79bde5
Fernando Alfaro <122326590+feralfaroh@users.noreply.github.com>
2025-03-07 14:57:48 -0600

    limpiando el init

--- function_app.py
+++ function_app.py
@@ -2,9 +2,6 @@ import azure.functions as func
 import logging
 import subprocess
 import sys
-import os
-
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'vendored'))
 
 app = func.FunctionApp()
 
