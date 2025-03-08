66cbf609c9b9e0671db6375d87fba659de71da31
Fernando Alfaro <122326590+feralfaroh@users.noreply.github.com>
2025-03-07 20:10:05 -0600

    CAMBIO DE ORDEN Y CORREGIMOS VERSIONES DE PIP

--- script1.py
+++ script1.py
@@ -1,8 +1,8 @@
 import time
 import os
-import pandas as pd
 import csv
 import numpy as np
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
